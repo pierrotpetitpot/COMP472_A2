7d9b6ce1c62d134d239140756d7101714465c3b2
RaduP13 <46818067+RaduP13@users.noreply.github.com>
2021-03-24 16:18:06 -0400

    Update main.py
    updated with object

--- main.py
+++ main.py
@@ -1,80 +1,116 @@
 from node import Node
 initial_state = [9,8,7,6,5,4,3,2,1]
 node = Node([None],initial_state)
-goal_state = [1,2,3,4,5,6,7,8,9]
 allChildren = []
 
-open = [initial_state]
-closed = []
 
 def getAllChildren(node:Node):
-    currentNode = node.state
-    tempChild = currentNode
+    currentNodeState = node.state
+    tempChildState = currentNodeState
     allChildren = []
     # child 0-1
-    tempChild[0], tempChild[1] = tempChild[1], tempChild[0]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[0], tempChildState[1] = tempChildState[1], tempChildState[0]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 1-2
-    tempChild[1], tempChild[2] = tempChild[2], tempChild[1]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[1], tempChildState[2] = tempChildState[2], tempChildState[1]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 0-3
-    tempChild[0], tempChild[3] = tempChild[3], tempChild[0]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[0], tempChildState[3] = tempChildState[3], tempChildState[0]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 1-4
-    tempChild[1], tempChild[4] = tempChild[4], tempChild[1]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[1], tempChildState[4] = tempChildState[4], tempChildState[1]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 2-5
-    tempChild[2], tempChild[5] = tempChild[5], tempChild[2]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[2], tempChildState[5] = tempChildState[5], tempChildState[2]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 3-4
-    tempChild[3], tempChild[4] = tempChild[4], tempChild[3]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[3], tempChildState[4] = tempChildState[4], tempChildState[3]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 4-5
-    tempChild[4], tempChild[5] = tempChild[5], tempChild[4]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[4], tempChildState[5] = tempChildState[5], tempChildState[4]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState 
     # child 3-6
-    tempChild[3], tempChild[6] = tempChild[6], tempChild[3]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[3], tempChildState[6] = tempChildState[6], tempChildState[3]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState 
     # child 4-7
-    tempChild[4], tempChild[7] = tempChild[7], tempChild[4]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[4], tempChildState[7] = tempChildState[7], tempChildState[4]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 5-8
-    tempChild[5], tempChild[8] = tempChild[8], tempChild[5]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[5], tempChildState[8] = tempChildState[8], tempChildState[5]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 6-7
-    tempChild[6], tempChild[7] = tempChild[7], tempChild[6]
-    allChildren.insert(tempChild)
-    tempChild = currentNode 
+    tempChildState[6], tempChildState[7] = tempChildState[7], tempChildState[6]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
     # child 7-8
-    tempChild[7], tempChild[8] = tempChild[8], tempChild[7]
-    allChildren.insert(tempChild)
-    tempChild = currentNode
+    tempChildState[7], tempChildState[8] = tempChildState[8], tempChildState[7]
+    newChild = Node(currentNodeState,tempChildState)
+    allChildren.insert(newChild)
+    tempChildState = currentNodeState
 
     return allChildren
 
-def getValidChildren(allChildren,closed):
+def getValidChildren(allChildren,closedList,openList):
     validChildren = []
     for child in allChildren:
         check = False
-        for visited in closed:
-            if(child == visited):
+        for visitedC in closedList:
+            if(child.state == visitedC.state):
+                check = True
+                break
+        for visitedO in openList:
+            if(child.state == visitedO.state):
                 check = True
                 break
         if(check == False):
-            validChildren.insert(visited)
+            validChildren.insert(child)
     return validChildren
 
-def compareGoal(currentNode:Node,goal_state:list):
+def compareGoal(currentNode:Node):
+    goal_state = [1,2,3,4,5,6,7,8,9]
     if(currentNode.state == goal_state):
         return True
     else: return False
+
+def depthFirstAlgorithm (initial_state:list):
+    initialNode = Node([None],initial_state)
+    openList = [initialNode]
+    closedList = []
+    solutionNode = Node([],[])
+    while (openList.count != 0):
+        x = openList.pop()
+        if(compareGoal(x)):
+            solutionNode.parent = x.parent
+            solutionNode.state = x.state
+        else:
+            allXChildren = getAllChildren(x)
+            closedList.append(x)
+            allValidXChildren = getValidChildren(allXChildren,closedList,openList)
+            for validChild in allValidXChildren:
+                openList.append(validChild)
+    
+    return Node([None],[None])
+
+
+
